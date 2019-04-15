3634299e93b6e5295b655cecb287cde85de06584
Zhian N. Kamvar <zkamvar@gmail.com>
2019-04-15 16:05:24 +0100

    bump

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 add_package_checks()
-
+# bump
 if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
