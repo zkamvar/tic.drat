a55ce75477963e5c13ea9acfc2388232d6a2b8fe
Zhian N. Kamvar <zkamvar@gmail.com>
2019-04-17 14:49:55 +0100

    attempt to fix things with a nasty hack

--- tic.R
+++ tic.R
@@ -1,6 +1,10 @@
 
 do_package_checks()
 
+os <- R.version$os
+
+tilde <- if (grepl("mingw", os)) "C:/Users/appveyor/Documents" else "~"
+
 if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
@@ -12,10 +16,10 @@ if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
 
   get_stage("deploy") %>%
     add_step(step_setup_push_deploy(
-      path = "~/git/drat",
+      path = file.path(tilde, "git/drat"),
       branch = "master",
       remote = paste0("git@github.com:", gsub("/.*$", "/tic.drat.repo", ci()$get_slug()), ".git")
     )) %>%
     add_step(step_add_to_drat()) %>%
-    add_step(step_do_push_deploy(path = "~/git/drat"))
+    add_step(step_do_push_deploy(path = file.path(tilde, "git/drat")))
 }
